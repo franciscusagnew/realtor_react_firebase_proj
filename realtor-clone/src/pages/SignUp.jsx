ff85a275548da127822ed58fd81b350fb57fecfa
Franciscus Agnew <franciscusagnew@users.noreply.github.com>
2024-11-14 15:51:02 -0500

    Added PrivateRoute and custom hook for Profile page and logging out

--- realtor-clone/src/pages/SignUp.jsx
+++ realtor-clone/src/pages/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import OAuth from "../components/OAuth";
