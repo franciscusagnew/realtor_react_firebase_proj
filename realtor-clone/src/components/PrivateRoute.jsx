e7477b31ad5d1922001f0b54e311e813b881463f
Franciscus Agnew <franciscusagnew@users.noreply.github.com>
2024-11-14 17:24:04 -0500

    Added Spinner component

--- realtor-clone/src/components/PrivateRoute.jsx
+++ realtor-clone/src/components/PrivateRoute.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router";
 import {useAuthStatus} from "../hooks/useAuthStatus";
+import Spinner from "./Spinner";
 
 export default function PrivateRoute() {
     // const loggedIn = false;
     const { loggedIn, loading } = useAuthStatus();
 
     if (loading) {
-        return <h3>Loading...</h3>
+        // return <h3>Loading...</h3>
+        return <Spinner />
     }
 
     return loggedIn ? <Outlet/> : <Navigate to="/sign-in"/>;
