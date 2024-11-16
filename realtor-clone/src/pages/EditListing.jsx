645c05af87f7bd3d29cf613f9ac2524a43be9a7a
Franciscus Agnew <franciscusagnew@users.noreply.github.com>
2024-11-16 16:55:12 -0500

    Create Edit Listing Page

--- realtor-clone/src/pages/EditListing.jsx
+++ realtor-clone/src/pages/EditListing.jsx
@@ -1,23 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Spinner from "../components/Spinner";
 import { toast } from "react-toastify";
 import {
-    getStorage,
+	getStorage,
 	ref,
 	uploadBytesResumable,
 	getDownloadURL,
 } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 import { v4 as uuidv4 } from "uuid";
-import {addDoc, collection, serverTimestamp } from "firebase/firestore";
-import { db } from "../firebase"
-import { useNavigate } from "react-router-dom";
+import {
+	addDoc,
+	collection,
+	doc,
+	getDoc,
+	serverTimestamp,
+	updateDoc,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { useNavigate, useParams } from "react-router-dom";
 
-export default function CreateListing() {
-    const navigate = useNavigate();
+export default function EditListing() {
+	const navigate = useNavigate();
 	const auth = getAuth();
 	const [geoLocationEnabled, setGeoLocationEnabled] = useState(true);
 	const [loading, setLoading] = useState(false);
+	const [listing, setListing] = useState(null);
 	const [formData, setFormData] = useState({
 		type: "rent",
 		name: "",
@@ -50,6 +58,32 @@ export default function CreateListing() {
 		longitude,
 		images,
 	} = formData;
+
+	const params = useParams();
+
+	useEffect(() => {
+		if (listing && listing.userRef !== auth.currentUser.uid) {
+			toast.error("You can't edit this listing!");
+			navigate("/");
+		}
+	}, [auth.currentUser.uid, listing, navigate]);
+
+	useEffect(() => {
+		setLoading(true);
+		async function fetchListing() {
+			const docRef = doc(db, "listings", params.listingId);
+			const docSnap = await getDoc(docRef);
+			if (docSnap.exists) {
+				setListing(docSnap.data());
+				setFormData({ ...docSnap.data() });
+				setLoading(false);
+			} else {
+				navigate("/");
+				toast.error("Listing does not exist!");
+			}
+		}
+		fetchListing();
+	}, [navigate, params.listingId]);
 
 	function onChange(e) {
 		let boolean = null;
@@ -170,14 +204,15 @@ export default function CreateListing() {
 			geolocation,
 			timestamp: serverTimestamp(),
 			userRef: auth.currentUser.uid,
-        };
-        delete formDataCopy.images;
-        !formDataCopy.offer && delete formDataCopy.discountedPrice;
-        delete formDataCopy.latitude;
-        delete formDataCopy.longitude;
-        const docRef = await addDoc(collection(db, "listings"), formDataCopy);
-        setLoading(false);
-        toast.success("Listing Created");
+		};
+		delete formDataCopy.images;
+		!formDataCopy.offer && delete formDataCopy.discountedPrice;
+		delete formDataCopy.latitude;
+		delete formDataCopy.longitude;
+		const docRef = doc(db, "listings", params.listingId);
+		await updateDoc(docRef, formDataCopy);
+		setLoading(false);
+		toast.success("Listing Edited");
 		navigate(`/category/${formDataCopy.type}/${docRef.id}`);
 	}
 
@@ -187,7 +222,7 @@ export default function CreateListing() {
 
 	return (
 		<main className="max-w-md px-2 mx-auto">
-			<h1 className="text-3xl text-center mt-6 font-bold">Create a Listing</h1>
+			<h1 className="text-3xl text-center mt-6 font-bold">Edit Listing</h1>
 			<form onSubmit={onSubmit}>
 				<p className="text-lg mt-6 font-semibold">Sell / Rent</p>
 				<div className="flex space-x-6">
@@ -454,7 +489,7 @@ export default function CreateListing() {
 					className="my-6 w-full bg-blue-600 text-white uppercase px-7 py-3 text-sm font-medium rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg focus:bg-blue-700 focus:shadow-lg active:bg-blue-800 active:shadow-lg"
 					type="submit"
 				>
-					Create Listing
+					Edit Listing
 				</button>
 			</form>
 		</main>
